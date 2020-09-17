56092afcf687a9eb5d7668591b7e1b3f01db79e2
AnatDean <anat.dean@hotmail.com>
2020-09-17 15:33:03 +0100

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getQuestions, getQuestionById } = require('./controllers');
 const { handleErrors } = require('./errors');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/api/questions', getQuestions);
 app.get('/api/questions/:question_id', getQuestionById);
